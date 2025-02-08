b3d7c06c9f35e527d08bdfefbdca8f536501063e
Stan-tab <godofwarmafia2@gmail.com>
2025-02-08 16:08:17 +0500

    feat: Add some action

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import "./style.css"
-import homePage from "./site-content.js"
+import {homePage, navBut} from "./site-content.js"
 
-homePage.render();
+const home = new homePage()
+
+navBut.forEach(el => {
+    const child = el.children[0];
+    el.addEventListener("click", () => {
+        navBut.forEach(element => {
+            const child = element.children[0];
+            child.classList = "drips";
+            element.classList = "";
+        });
+        el.classList = "visible";
+        child.classList = "visible drips"
+        if(el.value == 0) {
+            home.render();
+        }
+    })
+});
+
+home.render();
--- src/site-content.js
+++ src/site-content.js
@@ -1,17 +1,18 @@
 const container = document.querySelector("#container");
-const mainBox = document.createElement("div");;
+const mainBox = document.createElement("div");
+const navBut = [...document.querySelectorAll("header>nav>ul>li")]
 mainBox.classList = "mainBox";
 container.appendChild(mainBox);
 
 class homePage{
-    divs = {
+    _divs = {
         opener : document.createElement("div"),
         card1 : document.createElement("div"),
         card2 : document.createElement("div"),
         card3 : document.createElement("div"),
     }
 
-    paras = {
+    __paras = {
         card1Para : document.createElement("p"),
         card2Para : [document.createElement("p"),
                      document.createElement("p"),
@@ -23,59 +24,58 @@ class homePage{
         card3Para : document.createElement("p"),
     }
 
-    opH1 = document.createElement("h1");
-    card1H3 = document.createElement("h3");
-    card2H2 = document.createElement("h2");
-    card3H2 = document.createElement("h2");
+    _opH1 = document.createElement("h1");
+    _card1H3 = document.createElement("h3");
+    _card2H2 = document.createElement("h2");
+    _card3H2 = document.createElement("h2");
 
-    givingProps() {
-        this.divs.opener.classList = "opener";
-        this.divs.card1.classList = "card";
-        this.divs.card2.classList = "card hours";
-        this.divs.card3.classList = "card location";
+    givingProps = (() => {
+        this._divs.opener.classList = "opener";
+        this._divs.card1.classList = "card";
+        this._divs.card2.classList = "card hours";
+        this._divs.card3.classList = "card location";
 
-        this.opH1.classList = "scary";
-        this.card1H3.classList = "scary";
-        this.card2H2.classList = "scary";
-        this.card3H2.classList = "scary";
+        this._opH1.classList = "scary";
+        this._card1H3.classList = "scary";
+        this._card2H2.classList = "scary";
+        this._card3H2.classList = "scary";
 
-        this.opH1.textContent = "Scary plate";
-        this.card1H3.textContent = "John";
-        this.card2H2.textContent = "Hours";
-        this.card3H2.textContent = "Location";
+        this._opH1.textContent = "Scary plate";
+        this._card1H3.textContent = "John";
+        this._card2H2.textContent = "Hours";
+        this._card3H2.textContent = "Location";
 
-        this.paras.card1Para.innerHTML = "<span class=\"scary\">Scary plate</span> has the best <span class=\"scary\">meat</span>! The atmosphere and customer service make you feel like you are sitting in the middle of the <span class=\"scary\">hell</span>, eating like a <span class=\"scary\">Satan</span>! This is exactly the kind of place that I like to return to <span class=\"scary\">again and again</span>.";
-        this.paras.card2Para[0].textContent = "Sunday: 8am - 8pm";
-        this.paras.card2Para[1].textContent = "Monday: 6am - 6pm";
-        this.paras.card2Para[2].textContent = "Tuesday: 6am - 6pm";
-        this.paras.card2Para[3].textContent = "Wednesday: 6am - 6pm";
-        this.paras.card2Para[4].textContent = "Thursday: 6am - 10pm";
-        this.paras.card2Para[5].textContent = "Friday: 6am - 10pm";
-        this.paras.card2Para[6].textContent = "Saturday: 8am - 10pm";
-        this.paras.card3Para.textContent = "Your Dreams";
-    }
+        this.__paras.card1Para.innerHTML = "<span class=\"scary\">Scary plate</span> has the best <span class=\"scary\">meat</span>! The atmosphere and customer service make you feel like you are sitting in the middle of the <span class=\"scary\">hell</span>, eating like a <span class=\"scary\">Satan</span>! This is exactly the kind of place that I like to return to <span class=\"scary\">again and again</span>.";
+        this.__paras.card2Para[0].textContent = "Sunday: 8am - 8pm";
+        this.__paras.card2Para[1].textContent = "Monday: 6am - 6pm";
+        this.__paras.card2Para[2].textContent = "Tuesday: 6am - 6pm";
+        this.__paras.card2Para[3].textContent = "Wednesday: 6am - 6pm";
+        this.__paras.card2Para[4].textContent = "Thursday: 6am - 10pm";
+        this.__paras.card2Para[5].textContent = "Friday: 6am - 10pm";
+        this.__paras.card2Para[6].textContent = "Saturday: 8am - 10pm";
+        this.__paras.card3Para.textContent = "Your Dreams";
 
-    render() {
-        this.givingProps();
+        // appending childs
+        this._divs.opener.appendChild(this._opH1);
+        this._divs.card1.appendChild(this.__paras.card1Para);
+        this._divs.card1.appendChild(this._card1H3);
         
-        this.divs.opener.appendChild(this.opH1);
-        
-        this.divs.card1.appendChild(this.paras.card1Para);
-        this.divs.card1.appendChild(this.card1H3);
-        
-        this.divs.card2.appendChild(this.card2H2);
-        this.paras.card2Para.forEach(el => {
-            this.divs.card2.appendChild(el);
+        this._divs.card2.appendChild(this._card2H2);
+        this.__paras.card2Para.forEach(el => {
+            this._divs.card2.appendChild(el);
         })
 
-        this.divs.card3.appendChild(this.card3H2);
-        this.divs.card3.appendChild(this.paras.card3Para);
+        this._divs.card3.appendChild(this._card3H2);
+        this._divs.card3.appendChild(this.__paras.card3Para);
+    }) ()
 
-        mainBox.appendChild(this.divs.opener);
-        mainBox.appendChild(this.divs.card1);
-        mainBox.appendChild(this.divs.card2);
-        mainBox.appendChild(this.divs.card3);
+    render() {
+        mainBox.replaceChildren("");
+        mainBox.appendChild(this._divs.opener);
+        mainBox.appendChild(this._divs.card1);
+        mainBox.appendChild(this._divs.card2);
+        mainBox.appendChild(this._divs.card3);
     }
 }
 
-export default new homePage();
+export {homePage, navBut};
