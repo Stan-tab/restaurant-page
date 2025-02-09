51f6a21c7c99d6f601ab185f6af4e89969168ecb
Stan-tab <godofwarmafia2@gmail.com>
2025-02-09 13:10:47 +0500

    Rename folder

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "docs"),
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   devtool: "eval-source-map",
